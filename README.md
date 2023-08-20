338f1640765de5a1f6b5f77fb6fe8d4899e0cf78
Wojciech Fajczyk <37833669+wfajczyk@users.noreply.github.com>
2023-08-20 23:19:07 +0200

    feature add functional test (#2)

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # Order Management
+This is a simple order management application. It allows to create orders with api.
+
+Endpoints:
+- POST `/order/create`
+- POST `/order/remove/{order}`
+- POST `/order/recreate/{order}`
+
+Where `{order}` is order id from database. Actually it is autoincrement number.
+
+To build application please use:
+```shell
+composer install
+composer build
+```
 
 To test application run:
-```php
+```shell
+composer test-build
 composer test
 ```
---
+++ tests/Functional/OrderCreateTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use App\Entity\Order;
+use App\Repository\OrderRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class OrderCreateTest extends WebTestCase
+{
+
+    public function testCreateOrder(): void
+    {
+        $client = static::createClient();
+
+        $data = [
+            'products' => [
+                [
+                    'id' => 5,
+                    'quantity' => 10,
+                ],
+                [
+                    'id' => 6,
+                    'quantity' => 11,
+                ],
+            ],
+        ];
+
+        $client->request(
+            'POST',
+            '/order/create',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($data, JSON_THROW_ON_ERROR),
+        );
+
+        self::assertResponseIsSuccessful();
+        self::assertJson($client->getResponse()->getContent());
+        $order = $this->getOrder((int) $client->getResponse()->getContent());
+        self::assertNotNull($order);
+    }
+
+    private function getOrder(int $id): ?Order
+    {
+        return self::getContainer()->get(OrderRepository::class)->find($id);
+    }
+}
